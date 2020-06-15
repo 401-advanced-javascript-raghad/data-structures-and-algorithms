8d4d4dc820856139c58fd5cbcee931f3e92e8f37
raghadalquran <raghadalquran1@gmail.com>
2020-06-15 16:35:51 +0300

    done

---
+++ BinaryTree/README.md
@@ -0,0 +1,17 @@
+# Breadth-first Traversal.
+We have to know how to deal with nodes, BinaryTree and Queue.
+
+## Challenge
+Write a breadth first traversal method which takes a Binary Tree as its unique input. Without utilizing any of the built-in methods available to your language, traverse the input tree using a Breadth-first approach, and return a list of the values in the tree in the order they were encountered.
+
+## Approach & Efficiency
+## Efficiency
+### Big O :
+
+- Binary Tree : Time --> O(n), Space --> O(n)
+- breadthFirst : Time --> O(n), Space --> O(n)
+
+
+## Solution
+- [whiteboard](../assets/depth-first.jpg)
+- [whiteboard-link](https://drive.google.com/file/d/1vIGcBMgb-z4byG_8tAlOqwzdaW-Jc9Wd/view?usp=sharing)
---
+++ BinaryTree/breadth-first.js
@@ -0,0 +1,70 @@
+'use strict';
+
+class Tree {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+class QueueNode {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+class Queue {
+  constructor() {
+    this.front = null;
+    this.rear = null;
+  }
+
+  enqueue(val) {
+    let newNode = new QueueNode(val);
+
+    if (!this.front && !this.rear) {
+      this.front = newNode;
+      this.rear = newNode;
+      return;
+    }
+
+    this.rear.next = newNode;
+    this.rear = newNode;
+  }
+
+  dequeue() {
+    if (!this.front && !this.rear) return;
+
+    let oldFront = this.front;
+    this.front = this.front.next;
+
+    oldFront.next = null;
+    return oldFront.val;
+
+  }
+}
+class BinaryTree {
+  constructor() {
+    this.root = null;
+  }
+  breadthFirst(root = this.root) {
+    let printedArr = [];
+
+    if (!root) return;
+
+    let q = new Queue();
+    q.enqueue(root);
+
+    while (q.front) {
+      if (q.front.val.left) q.enqueue(q.front.val.left);
+      if (q.front.val.right) q.enqueue(q.front.val.right);
+
+      let removedItem = q.dequeue();
+      printedArr.push(removedItem.val);
+    }
+
+    return printedArr;
+  }
+}
+
+module.exports = { Tree, QueueNode, BinaryTree };
